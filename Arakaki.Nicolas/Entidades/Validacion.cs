5d800b20c93f7cc12bd3e20908751a0918283083
NicolasArak <nicolasarakaki@users.noreply.github.com>
2020-10-05 21:11:13 -0300

    Parcial 1 Laboratorio II

---
+++ Arakaki.Nicolas/Entidades/Validacion.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public static class Validacion
+    {
+        /// <summary>
+        /// Valida si el string pasado por parámetro es un número entero.
+        /// </summary>
+        /// <param name="numString"></param>
+        /// <returns>bool: retorna true si el string es un número entero, caso contrario false</returns>
+        public static bool EsNumeroInt(string numString)
+        {
+            bool retorno = true;
+
+            foreach (char caracter in numString)
+            {
+                if (!char.IsDigit(caracter))
+                {
+                    retorno = false;
+                    break;
+                }
+            }
+
+            return retorno;
+
+        }
+
+        /// <summary>
+        /// Valida si el string pasado por parámetro es un número decimal.
+        /// </summary>
+        /// <param name="numString"></param>
+        /// <returns>retorna true si el string es un número decimal, caso contrario false</returns>
+        public static bool EsNumeroDouble(string numString)
+        {
+            bool retorno = false;
+
+            if (numString.Contains(","))
+            {
+                numString = numString.Replace(",", "");
+            }
+            if (numString.Contains("."))
+            {
+                numString = numString.Replace(".", "");
+            }
+
+            retorno = EsNumeroInt(numString);
+
+            return retorno;
+
+        }
+
+        /// <summary>
+        /// Valida si el string pasado por parámetro no contiene números.
+        /// </summary>
+        /// <param name="texto"></param>
+        /// <returns>bool: retorna true si el string es sólo texto, caso contrario false</returns>
+        public static bool EsString(string texto)
+        {
+            bool retorno = true;
+
+            foreach (char caracter in texto)
+            {
+                if (char.IsDigit(caracter))
+                {
+                    retorno = false;
+                    break;
+                }
+            }
+
+            return retorno;
+        }
+
+    }
+}
